0b60353492f4e35794a4e776e13f159345123a02
Lucas Pimentel <lucaspimentel@gmail.com>
2026-02-23 21:57:31 -0500

    delete unused script

--- kill-port.ps1
+++
@@ -1,5 +0,0 @@
-$conns = Get-NetTCPConnection -LocalPort 4567 -ErrorAction SilentlyContinue
-foreach ($c in $conns) {
-    Stop-Process -Id $c.OwningProcess -Force -ErrorAction SilentlyContinue
-}
-Write-Host "Done"
